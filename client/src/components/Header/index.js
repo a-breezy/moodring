b6254328bbc2193ab8c2258b7218586e7ac2720d
a.breezy <ambrose.wilkins@gmail.com>
2023-06-23 15:37:43 -0400

    updated header and footer

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+	return (
+		<header className="header">
+			<h1>MoodRing</h1>
+			<h2>
+				Connect your Oura Ring
+				<br />
+				Learn what habits work
+			</h2>
+		</header>
+	);
+}
+
+export default Header;
