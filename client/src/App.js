304924a078178e53bde0bb795527bbfe657168d0
a.breezy <ambrose.wilkins@gmail.com>
2023-06-23 14:33:29 -0400

    created frontend folders and started working on components

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="header">
+				<div>
+					<h1>MoodRing</h1>
+					<h2>a way to connect your device and see your gains</h2>
+				</div>
+			</header>
+		</div>
+	);
+}
+
+export default App;
