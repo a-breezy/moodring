8f9af79f6b9bc33ac7b23a9184c4c1f19f81930a
a.breezy <ambrose.wilkins@gmail.com>
2023-06-26 13:06:08 -0400

    created outline of moodstats component with col for stats and the outline of where the graph will go

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,16 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MoodStats from "./components/MoodStats";
 
 function App() {
 	return (
 		<div className="App">
 			<Header></Header>
+			{/* This area will render with react router to provide routes to various pages */}
+			<div>
+				<MoodStats />
+			</div>
 			<Footer></Footer>
 		</div>
 	);
