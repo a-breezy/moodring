00c07455667999849151adf960ffef6f3f032e68
a.breezy <ambrose.wilkins@gmail.com>
2023-07-28 18:22:43 -0400

    created and styled log mood page

--- client/src/components/LogMood/index.js
+++ client/src/components/LogMood/index.js
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 // this component taks the user's data from oura and adds a mood and description to it
 function LogMood() {
+	const [dropDownOpen, setDropDownOpen] = useState(false);
+
+	let moods = [
+		"Ecstatic",
+		"Happy",
+		"Relaxed",
+		"Indifferent",
+		"Bored",
+		"Sad",
+		"Stressed",
+		"Anxious",
+		"Angry",
+	];
+
+	const handleOpen = () => {
+		setDropDownOpen(!dropDownOpen);
+	};
+
 	return (
 		<section id="log-mood-section">
-			<div className="log-mood-container">
-				<div className="mood-select">
-					<h3>Log a New Mood</h3>
-					{/* insert a drop down menu here */}
+			<h2>Log a New Mood</h2>
+			<p className="log-mood-desc">Here is where you describe your mood</p>
+			<form className="log-mood-container">
+				<div className="date-item log-container">
+					<h3>Select a date, if not today</h3>
+					<p>This is the calendar container</p>
 				</div>
-				<div className="notes">
-					<h3>Notes:</h3>
-					<textarea rows="4"></textarea>
+				<div className="mood-select-item log-container">
+					<h3>Select a Mood from the Drop-Down</h3>
+					<div>
+						<p>This is the mood selection container</p>
+						<button onClick={handleOpen}>Select Mood</button>
+						{dropDownOpen ? (
+							<ul className="mood-dropdown">
+								{moods.map((mood) => {
+									return (
+										<li className="mood-dropdown-item" key={mood}>
+											<button>{mood}</button>
+										</li>
+									);
+								})}
+							</ul>
+						) : null}
+					</div>
 				</div>
-			</div>
+				<div className="notes-item log-container">
+					<div>
+						<h3>Notes:</h3>
+						<p>
+							Here's where you can add notes related to the mood that you're
+							feeling. It will be logged and can be referenced in the future.
+						</p>
+					</div>
+					<textarea rows="4" placeholder="Type your mood here"></textarea>
+				</div>
+			</form>
 		</section>
 	);
 }
