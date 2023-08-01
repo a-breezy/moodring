a53f50d6d7cbb155854db9507612b84c05e3a5f5
a.breezy <ambrose.wilkins@gmail.com>
2023-08-01 16:56:03 -0400

    moved all the homepage stuff to profile page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MoodStats from "../components/MoodStats";
+import LogMood from "../components/LogMood";
+
+const Profile = () => {
+	return (
+		<>
+        {/* if logged in user should see stats and be able to log mood */}
+			<MoodStats />
+			<LogMood />
+        {/* otherwise user should see a welcome screen and a call to action */}
+		</>
+	);
+};
+
+export default Profile;
